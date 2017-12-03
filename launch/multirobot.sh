fe2febd70b29d7732a8ae12807ee4c358b602d5f
jposton <jposton@nevada.unr.edu>
2017-12-03 11:59:02 -0800

    added turtle Target

--- launch/multirobot.sh
+++ launch/multirobot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-roslaunch multirobotPID.launch turtle2x:=$4 turtle2y:=$5 turtle2theta:=$6 &
+roslaunch multirobotPID.launch turtle2x:=$6 turtle2y:=$7 turtle2theta:=$8 &
 sleep 1s
 roslaunch teleportpenset.launch turtle1x:=$1 turtle1y:=$2 turtle1theta:=$3 &
 sleep 1s
-roslaunch penset.launch
+roslaunch penset.launch target1x:=$4 target1y:=$5 target2x:=$9 target2y:=$10
